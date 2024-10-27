834d935472008f5b6b53c7bbf3455adf592948eb
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-10-27 20:41:07 +0600

    updated Landing modules

--- src/components/modules/home/Landing.tsx
+++ src/components/modules/home/Landing.tsx
@@ -1,9 +1,22 @@
 import { Input } from "@nextui-org/input";
+import { SearchIcon } from "../../icons";
 
 const Landing = () => {
   return (
     <div className="h-[calc(100vh-64px)] bg-[url('/glass.jpg')] bg-cover bg-center">
-      <Input />
+      <div className="py-16 px-2 mx-auto max-w-xl text-center">
+        <form action="">
+          <Input
+            placeholder="Search"
+            classNames={{
+              inputWrapper: " mx-auto bg-default",
+            }}
+            size="lg"
+            startContent={<SearchIcon className="w-6 h-6 text-gray-500" />}
+            type="text"
+          />
+        </form>
+      </div>
     </div>
   );
 };
