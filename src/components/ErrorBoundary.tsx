c481c0da34e13e0d6dff40652aaa41678326e636
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-11-14 21:21:14 +0600

    added custom ErrorBoundary to handle particuler section error

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Component } from "react";
+
+interface ErrorBoundaryProps {
+  fallback: React.ReactNode;
+  children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
