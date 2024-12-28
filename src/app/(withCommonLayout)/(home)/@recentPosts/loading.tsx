f7a453a2749b2697d0e575faf40a5749b0b135a8
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-12-28 22:55:53 +0600

    added parallal routes for recentPosts

---
+++ src/app/(withCommonLayout)/(home)/@recentPosts/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h3>Loading...</h3>
+    </div>
+  );
+};
+
+export default Loading;
--- src/app/(withCommonLayout)/(home)/@recentPosts/page.tsx
+++ src/app/(withCommonLayout)/(home)/@recentPosts/page.tsx
@@ -1,8 +1,8 @@
+import Card from "@/src/components/ui/Card";
+import Container from "@/src/components/ui/Container";
 import getRecentPosts from "@/src/services/RecentPosts";
 import { IPost } from "@/src/types";
 import Link from "next/link";
-import Card from "../../ui/Card";
-import Container from "../../ui/Container";
 
 const RecentPost = async () => {
   const { data: posts } = await getRecentPosts();
@@ -15,12 +15,12 @@ const RecentPost = async () => {
       <p className="mb-6 text-gray-600 dark:text-gray-300 text-center">
         Check out our latest posts below:
       </p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
         {posts.map((post: IPost) => (
           <Card key={post._id} post={post} />
         ))}
       </div>
-      <div className="flex justify-center mt-6">
+      <div className="flex justify-center my-6">
         <Link href={"/found-items"}>
           <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 dark:bg-blue-700 dark:hover:bg-blue-800">
             See All Posts
